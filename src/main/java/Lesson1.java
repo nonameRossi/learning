53ec301681bfc36649f4340d4418790438f10dfa
Valiantsin_Samakhave <Valiantsin_Samakhavets@epam.com>
2018-09-19 21:40:16 +0300

    The first commit

---
+++ src/main/java/Lesson1.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Valiantsin_Samakhave on 9/19/2018.
+ */
+public class Lesson1 {
+    public static void main(String[] args) {
+        task1();
+    }
+    private static void task1(){
+        System.out.println("Hello World!");
+    }
+}
